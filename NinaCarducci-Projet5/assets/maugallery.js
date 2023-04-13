fbc15d635559a14f1f8799efbe66833b42c11390
zahra4221 <shakilzahra1@gmail.com>
2023-04-13 16:44:01 +0200

    correction couleur filtre

--- NinaCarducci-Projet5/assets/maugallery.js
+++ NinaCarducci-Projet5/assets/maugallery.js
@@ -235,12 +235,29 @@
         console.error(`Unknown tags position: ${position}`);
       }
     },
+    /*
     filterByTag() {
       if ($(this).hasClass("active-tag")) {
         return;
       }
       $(".active-tag").removeClass("active active-tag");
       $(this).addClass("active-tag");
+
+      var tag = $(this).data("images-toggle");
+
+      $(".gallery-item").each(function () {
+        $(this).parents(".item-column").hide();
+        if (tag === "all") {
+          $(this).parents(".item-column").show(300);
+        } else if ($(this).data("gallery-tag") === tag) {
+          $(this).parents(".item-column").show(300);
+        }*/
+    filterByTag() {
+      if ($(this).hasClass("active")) {
+        return;
+      }
+      $(".nav-link").removeClass("active");
+      $(this).addClass("active");
 
       var tag = $(this).data("images-toggle");
 
